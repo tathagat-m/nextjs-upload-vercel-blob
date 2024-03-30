9b422a7f01daf65c8d923072a5930f05a629d18b
Tathagat Mohanty <tathagat.mohanty@reevoy.com>
2024-03-30 20:54:04 +0530

    initial setup

---
+++ src/components/UploadFile/UploadFile.jsx
@@ -0,0 +1,22 @@
+import { put } from "@vercel/blob";
+import { revalidatePath } from "next/cache";
+
+export async function Form() {
+  async function uploadImage(formData) {
+    "use server";
+    const imageFile = formData.get("image");
+    const blob = await put(imageFile.name, imageFile, {
+      access: "public",
+    });
+    revalidatePath("/");
+    return blob;
+  }
+
+  return (
+    <form action={uploadImage}>
+      <label htmlFor="image">Image</label>
+      <input type="file" id="image" name="image" required />
+      <button>Upload</button>
+    </form>
+  );
+}
